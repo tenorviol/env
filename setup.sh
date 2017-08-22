0d38f9e81c9d42f2aa474f403d19c2bdb6052439
Chris Johnson <chris@eero.com>
2017-08-22 14:22:48 -0700

    Removed a bunch of brew installs that strike me as unnecessary

--- setup.sh
+++ setup.sh
@@ -19,33 +19,18 @@ if test ! $(which brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Update homebrew recipes
 brew update
 
-# Install GNU core utilities (those that come with OS X are outdated)
-brew install coreutils
-
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
-
-# Install Bash 4
-brew install bash
-
-# Install more recent versions of some OS X tools
-brew tap homebrew/dupes
-brew install homebrew/dupes/grep
-
-binaries=(
+stuff=(
   ag
   git
   node
   postgres
   python3
   scala
-  typesafe-activator
 )
 
-echo "installing binaries..."
-brew install ${binaries[@]}
+echo "installing stuff..."
+brew install ${stuff[@]}
 
 brew cleanup
